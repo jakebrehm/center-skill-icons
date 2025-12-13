773d96e62a1b6355dd19eec25571832e0df893e5
Jake Brehm <jake.m.brehm@gmail.com>
2025-12-13 15:30:03 -0500

    Add document comments in CenterSkillIconsPlugin.java

--- src/main/java/com/centerskillicons/CenterSkillIconsPlugin.java
+++ src/main/java/com/centerskillicons/CenterSkillIconsPlugin.java
@@ -31,6 +31,10 @@ public class CenterSkillIconsPlugin extends Plugin
 	@Inject private Client client;
 	@Inject private ClientThread clientThread;
 
+	/**
+	 * Initialized up the plugin when it is enabled.
+	 * Stores the original positions of the skill icons and sets them to their centered positions.
+	 */
 	@Override
 	protected void startUp() throws Exception
 	{
@@ -38,12 +42,20 @@ public class CenterSkillIconsPlugin extends Plugin
 		clientThread.invoke(this::updatePositions);
 	}
 
+	/**
+	 * Cleans up the plugin when it is disabled.
+	 * Resets all skill icons to their original positions.
+	 */
 	@Override
 	protected void shutDown() throws Exception
 	{
 		clientThread.invoke(this::resetPositions);
 	}
 
+	/**
+	 * Update icon positions when skills panel-related scripts are executed.
+	 * @param event The script post-fired event.
+	 */
 	@Subscribe
 	public void onScriptPostFired(ScriptPostFired event) {
 		storeCurrentPositions();
@@ -55,6 +67,12 @@ public class CenterSkillIconsPlugin extends Plugin
 		}
 	}
 
+	/**
+	 * Captures and stores the original positions of all skill icons in the skills panel.
+	 * The overall intention is to only store positions once on the first call or after being reset, and to prohibit
+	 * the execution of other operations until these positions have been stored.
+	 * Validates that positions for all skills were found before committing the values to storage.
+	 */
 	private void storeCurrentPositions() {
 		if (originalPositions != null) return;
 
@@ -76,27 +94,49 @@ public class CenterSkillIconsPlugin extends Plugin
 		}
 	}
 
+	/**
+	 * Sets the icons in the skills panel to their default corrected positions.
+	 * The default positions are specified in the SkillData enum.
+	 */
 	private void updatePositions() {
 		if (originalPositions == null) return;
 		setPositions(SkillData.mapping());
 	}
 
+	/**
+	 * Sets the icons in the skills panel to their original positions.
+	 */
 	private void resetPositions() {
 		if (originalPositions == null) return;
 		setPositions(originalPositions);
 		originalPositions = null;
 	}
 
+	/**
+	 * Extracts the child ID from the ID of its parent widget.
+	 * @param id The parent widget's ID.
+	 * @return The child widget's ID.
+	 */
 	private int getChildId(int id) {
 		return id & WIDGET_CHILD_ID_MASK;
 	}
 
+	/**
+	 * Changes a widget's original X- and Y-coordinates and revalidates.
+	 * @param widget The widget to move.
+	 * @param x The new x-coordinate.
+	 * @param y The new y-coordinate.
+	 */
 	private void moveWidget(Widget widget, int x, int y) {
 		widget.setOriginalX(x);
 		widget.setOriginalY(y);
 		widget.revalidate();
 	}
 
+	/**
+	 * Sets the positions of the icons in the skills panels to the coordinates specified in the mapping.
+	 * @param mapping A hashmap mapping skills to their target coordinates.
+	 */
 	private void setPositions(HashMap<Skill, Coordinate> mapping) {
 		Widget skillsPanel = client.getWidget(InterfaceID.Stats.UNIVERSE);
 		if (skillsPanel == null) return;
